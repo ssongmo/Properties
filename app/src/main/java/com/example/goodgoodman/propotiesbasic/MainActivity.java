d6b041533eefa074227523cd33d2b50049926746
ssongmo <heosongmoo@naver.com>
2017-03-21 11:56:45 +0900

    Properties

---
+++ app/src/main/java/com/example/goodgoodman/propotiesbasic/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.goodgoodman.propotiesbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editName;
+    Switch switchOnOff;
+
+    RelativeLayout layout;
+    PropertyUtil propertyUtil;
+    final String propertyFile = "test.properties";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        propertyUtil = propertyUtil.getInstance(this);
+
+        editName = (EditText)findViewById(R.id.editText);
+        switchOnOff = (Switch)findViewById(R.id.switchOn);
+
+        layout = (RelativeLayout)findViewById(R.id.layout2);
+
+        if("false".equals(propertyUtil.getProperty("firstOpen"))){
+            layout.setVisibility(View.GONE);
+        }
+    }
+
+    public void closeHelp(View view){
+        layout.setVisibility(View.GONE);
+        propertyUtil.saveProperty("firstOpen", "false");
+    }
+    public void saveSetting(View view){
+
+    }
+
+
+
+
+}
